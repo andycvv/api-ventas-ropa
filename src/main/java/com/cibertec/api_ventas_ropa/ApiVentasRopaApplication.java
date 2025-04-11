4d8821f52abbd5ad2d1ded7fbf89a437e1ad9e24
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-11 18:17:13 -0500

    agregar proyecto

---
+++ src/main/java/com/cibertec/api_ventas_ropa/ApiVentasRopaApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.api_ventas_ropa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVentasRopaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVentasRopaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/api_ventas_ropa/ApiVentasRopaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.api_ventas_ropa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVentasRopaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

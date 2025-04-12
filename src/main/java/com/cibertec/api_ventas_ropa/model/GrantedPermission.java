74d1ed023338cda40df1cc1eb57bb0c79dd5c38e
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-12 07:34:43 -0500

    agregar entidad y relacion GrantedPermission

--- src/main/java/com/cibertec/api_ventas_ropa/model/GrantedPermission.java
+++ src/main/java/com/cibertec/api_ventas_ropa/model/GrantedPermission.java
@@ -1,25 +1,26 @@
 package com.cibertec.api_ventas_ropa.model;
 
-import java.util.List;
-
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Role {
+public class GrantedPermission {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private String name;
 	
-	@OneToMany(fetch = FetchType.EAGER, mappedBy = "role")
-	private List<GrantedPermission> permissions;
+	@ManyToOne
+	@JoinColumn(name = "role_id")
+	private Role role;
+	@ManyToOne
+	@JoinColumn(name = "permission_id")
+	private Permission permission;
 }

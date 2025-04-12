910b3620e7471aa517e275c508f1d68f47796191
tks1996 <lamm180606@gmail.com>
2025-04-12 11:45:42 -0500

    agregar entidad Color

---
+++ src/main/java/com/cibertec/api_ventas_ropa/model/Color.java
@@ -0,0 +1,19 @@
+package com.cibertec.api_ventas_ropa.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Color {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nombre;
+	private String code;
+}

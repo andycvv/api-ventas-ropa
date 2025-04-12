ed2e20692ea39232e4e1f2332ec71bb8d8ac943c
TovarPiero <pierotovar21@gmail.com>
2025-04-12 11:27:54 -0500

    agregar entidad entidad ImageProduct

---
+++ src/main/java/com/cibertec/api_ventas_ropa/model/ImageProduct.java
@@ -0,0 +1,25 @@
+package com.cibertec.api_ventas_ropa.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class ImageProduct {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String url;
+	@ManyToOne
+	@JoinColumn(name = "inventory_id")
+	private Inventory inventory;
+	
+
+}

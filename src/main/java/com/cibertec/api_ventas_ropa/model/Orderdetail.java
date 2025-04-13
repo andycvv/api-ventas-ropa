455cf920fb1251b2b3564d99cbd36df58c1007ed
Yadira Cceline Auccasi Loa <yadira.auccasiloa@gmail.com>
2025-04-12 20:43:30 -0500

    agregar entidad Orderdetail

---
+++ src/main/java/com/cibertec/api_ventas_ropa/model/Orderdetail.java
@@ -0,0 +1,20 @@
+package com.cibertec.api_ventas_ropa.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Orderdetail {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private int quantity;
+	private double price;
+}

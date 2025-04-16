4acd418341fdb41521fd15c5d828776a1ef24682
TovarPiero <pierotovar21@gmail.com>
2025-04-16 12:18:18 -0500

    agregar findAll y save a SizeController

--- src/main/java/com/cibertec/api_ventas_ropa/controller/SizeController.java
+++ src/main/java/com/cibertec/api_ventas_ropa/controller/SizeController.java
@@ -1,9 +1,15 @@
 package com.cibertec.api_ventas_ropa.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cibertec.api_ventas_ropa.model.Size;
 import com.cibertec.api_ventas_ropa.service.SizeService;
 
 @RestController
@@ -11,5 +17,13 @@ import com.cibertec.api_ventas_ropa.service.SizeService;
 public class SizeController {
 	@Autowired
 	private SizeService service;
-
+	
+	@GetMapping
+	public List<Size> findAll(){
+		return service.findAll();
+	}
+	@PostMapping
+	public void save(@RequestBody Size size) {
+		service.save(size);
+	}
 }

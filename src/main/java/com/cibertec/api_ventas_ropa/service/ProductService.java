c83763a4c416a2aae1890f8fe8de6043b2c9c9f7
Brahayan05 <brahayanjw@gmail.com>
2025-04-16 12:57:53 -0500

    agregar findAll y save a ProductService

---
+++ src/main/java/com/cibertec/api_ventas_ropa/service/ProductService.java
@@ -0,0 +1,23 @@
+package com.cibertec.api_ventas_ropa.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cibertec.api_ventas_ropa.model.Product;
+import com.cibertec.api_ventas_ropa.repository.ProductRepository;
+
+@Service
+public class ProductService {
+	@Autowired
+	private ProductRepository repo;
+
+	public List<Product> findAll() {
+		return repo.findAll();
+	}
+	public void save(Product product) {
+		repo.save(product);
+	}
+}
+

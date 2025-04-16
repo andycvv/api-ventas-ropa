131e649a03863d2fe650b31b0152e2ea71cef6c4
tks1996 <lamm180606@gmail.com>
2025-04-16 11:38:07 -0500

    agregar findAll y save a CategoryController

--- src/main/java/com/cibertec/api_ventas_ropa/controller/CategoryController.java
+++ src/main/java/com/cibertec/api_ventas_ropa/controller/CategoryController.java
@@ -1,10 +1,17 @@
 package com.cibertec.api_ventas_ropa.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cibertec.api_ventas_ropa.model.Category;
 import com.cibertec.api_ventas_ropa.service.CategoryService;
+
 
 @RestController
 @RequestMapping("/category")
@@ -13,5 +20,13 @@ public class CategoryController {
 	@Autowired
 	private CategoryService service;
 	
-
+	@GetMapping
+	public List<Category> findAll(){
+		return service.findAll();
+	}
+	
+	@PostMapping
+	public void save(@RequestBody Category category) {
+		service.save(category);
+	}
 }

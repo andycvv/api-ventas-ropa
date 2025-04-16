71ab2f07f0bc2cb464c0213e3a48fcf7475eb214
tks1996 <lamm180606@gmail.com>
2025-04-16 11:15:50 -0500

    agregar findAll y save en CategoryService

--- src/main/java/com/cibertec/api_ventas_ropa/service/CategoryService.java
+++ src/main/java/com/cibertec/api_ventas_ropa/service/CategoryService.java
@@ -1,8 +1,11 @@
 package com.cibertec.api_ventas_ropa.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cibertec.api_ventas_ropa.model.Category;
 import com.cibertec.api_ventas_ropa.repository.CategoryRepository;
 
 @Service
@@ -10,5 +13,13 @@ public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repo;
+	
+	public List<Category> findAll(){
+		return repo.findAll();
+	}
+	
+	public void save(Category category) {
+		repo.save(category);
+	}
 	
 }
--- src/main/java/com/cibertec/api_ventas_ropa/service/ColorService.java
+++ src/main/java/com/cibertec/api_ventas_ropa/service/ColorService.java
@@ -1,6 +1,5 @@
 package com.cibertec.api_ventas_ropa.service;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;

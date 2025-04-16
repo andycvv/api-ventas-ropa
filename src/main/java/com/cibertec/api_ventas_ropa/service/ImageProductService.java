29c596cd2170046c0e957840d2392f2218b6252c
TovarPiero <pierotovar21@gmail.com>
2025-04-16 12:01:17 -0500

    agregar findAll y  save en ImageProductServices

--- src/main/java/com/cibertec/api_ventas_ropa/service/ImageProductService.java
+++ src/main/java/com/cibertec/api_ventas_ropa/service/ImageProductService.java
@@ -1,8 +1,12 @@
 package com.cibertec.api_ventas_ropa.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cibertec.api_ventas_ropa.model.Color;
+import com.cibertec.api_ventas_ropa.model.ImageProduct;
 import com.cibertec.api_ventas_ropa.repository.ImageProductrepository;
 
 @Service
@@ -10,6 +14,12 @@ public class ImageProductService {
 	
 	@Autowired
 	private ImageProductrepository repo;
-	
+	public List<ImageProduct> findAll(){
+		return repo.findAll();
+		
+	}
+	public void save(ImageProduct imageProduct) {
+		repo.save(imageProduct);
 
+	}
 }

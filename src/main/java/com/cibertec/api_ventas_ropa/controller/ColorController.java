c56d962a62159700ec341c3790e5f06f61cd2f84
tks1996 <lamm180606@gmail.com>
2025-04-16 11:37:50 -0500

    agregar findAll y save a ColorController

--- src/main/java/com/cibertec/api_ventas_ropa/controller/ColorController.java
+++ src/main/java/com/cibertec/api_ventas_ropa/controller/ColorController.java
@@ -1,9 +1,15 @@
 package com.cibertec.api_ventas_ropa.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cibertec.api_ventas_ropa.model.Color;
 import com.cibertec.api_ventas_ropa.service.ColorService;
 
 @RestController
@@ -13,4 +19,13 @@ public class ColorController {
 	@Autowired
 	private ColorService service;
 	
+	@GetMapping
+	public List<Color> findAll(){
+		return service.findAll();
+	}
+	
+	@PostMapping
+	public void save(@RequestBody Color color) {
+		service.save(color);
+	}
 }

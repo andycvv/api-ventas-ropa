6fba7f79a0e4125ce4032a29e8b3726967ae988f
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-16 10:19:54 -0500

    agregar @JsonIgnore en addresses y orders en User

--- src/main/java/com/cibertec/api_ventas_ropa/model/User.java
+++ src/main/java/com/cibertec/api_ventas_ropa/model/User.java
@@ -2,6 +2,8 @@ package com.cibertec.api_ventas_ropa.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -30,8 +32,10 @@ public class User {
 	private Role role;
 	
 	@OneToMany(mappedBy = "userId")
+	@JsonIgnore
 	private List<Address> addresses;
 	
 	@OneToMany(mappedBy = "user")
-	private List<Order> Orders;
+	@JsonIgnore
+	private List<Order> orders;
 }

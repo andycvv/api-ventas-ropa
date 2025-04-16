ea1dcf96087886bc351d4f47660b10e88c9f8f1b
tks1996 <lamm180606@gmail.com>
2025-04-16 11:12:59 -0500

    agregar findAll y save en ColorService

--- src/main/java/com/cibertec/api_ventas_ropa/service/ColorService.java
+++ src/main/java/com/cibertec/api_ventas_ropa/service/ColorService.java
@@ -1,8 +1,12 @@
 package com.cibertec.api_ventas_ropa.service;
 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cibertec.api_ventas_ropa.model.Color;
 import com.cibertec.api_ventas_ropa.repository.ColorRepository;
 
 @Service
@@ -11,4 +15,11 @@ public class ColorService {
 	@Autowired
 	private ColorRepository repo;
 	
+	public List<Color> findAll(){
+		return repo.findAll();
+	}
+	
+	public void save(Color color) {
+		repo.save(color);
+	}
 }

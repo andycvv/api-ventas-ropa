d7cdd6edc5fb7c93e7310aecabf6e4fb98d6d008
Brahayan05 <brahayanjw@gmail.com>
2025-04-16 12:54:46 -0500

    agregar @Column(updatable = false) al atributo createdAt

--- src/main/java/com/cibertec/api_ventas_ropa/model/Product.java
+++ src/main/java/com/cibertec/api_ventas_ropa/model/Product.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,6 +27,7 @@ public class Product {
 	private String description;
 	private double price;
 	@CreationTimestamp
+	@Column(updatable = false)
 	private LocalDateTime createdAt;
     @OneToMany(mappedBy = "product")
     @JsonIgnore

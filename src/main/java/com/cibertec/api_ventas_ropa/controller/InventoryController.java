6fb037ad6870a86e6d43b2d390488aa8ea1020a2
Brahayan05 <brahayanjw@gmail.com>
2025-04-16 17:28:42 -0500

    agregar findAll y save en InventoryController

---
+++ src/main/java/com/cibertec/api_ventas_ropa/controller/InventoryController.java
@@ -0,0 +1,30 @@
+package com.cibertec.api_ventas_ropa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cibertec.api_ventas_ropa.model.Inventory;
+import com.cibertec.api_ventas_ropa.service.InventoryService;
+
+@RestController
+@RequestMapping("/inventory")
+public class InventoryController {
+	@Autowired
+	private InventoryService service;
+
+	@GetMapping
+	public List<Inventory> findAll() {
+		return service.findAll();
+	}
+
+	@PostMapping
+	public void save(@RequestBody Inventory inventory) {
+		service.save(inventory);
+	}
+}

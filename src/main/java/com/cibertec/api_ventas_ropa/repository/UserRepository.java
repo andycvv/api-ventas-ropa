35f72b0323bb0061a9c47ec7a5b30ea400bcdf2d
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-25 11:53:07 -0500

    agregar findByEmail en UserRepository

--- src/main/java/com/cibertec/api_ventas_ropa/repository/UserRepository.java
+++ src/main/java/com/cibertec/api_ventas_ropa/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.cibertec.api_ventas_ropa.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cibertec.api_ventas_ropa.model.User;
+import java.util.List;
+
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+	Optional<User> findByEmail(String username);
 }

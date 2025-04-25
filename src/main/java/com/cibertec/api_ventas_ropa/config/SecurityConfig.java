edff1776268922ff1abe10b71483d7a05553b82d
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-25 11:52:30 -0500

    crear SecurityConfig

---
+++ src/main/java/com/cibertec/api_ventas_ropa/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.cibertec.api_ventas_ropa.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+import com.cibertec.api_ventas_ropa.service.UserDetailsServiceImpl;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+         .csrf(csrf -> csrf.disable())
+         .authorizeHttpRequests(auth -> auth
+           .requestMatchers("/color/**", "/category/**", "/product/**", "/size/**").hasRole("ADMIN")
+           .requestMatchers("/auth/**").permitAll()
+           .anyRequest().authenticated()
+         )
+         .cors(Customizer.withDefaults())
+         .httpBasic(Customizer.withDefaults());
+		
+		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
+	}
+}

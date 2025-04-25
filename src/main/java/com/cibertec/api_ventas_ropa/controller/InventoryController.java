8d9bf8ca4239ba01f2498cf22fe07005a7695062
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-25 11:53:43 -0500

    agregar permisos a los endpoints en InventoryController y OrderController segun el rol

--- src/main/java/com/cibertec/api_ventas_ropa/controller/InventoryController.java
+++ src/main/java/com/cibertec/api_ventas_ropa/controller/InventoryController.java
@@ -3,6 +3,7 @@ package com.cibertec.api_ventas_ropa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,16 +20,19 @@ public class InventoryController {
 	@Autowired
 	private InventoryService service;
 
+	@PreAuthorize("hasAnyRole('ADMIN', 'CLIENTE')")
 	@GetMapping
 	public List<Inventory> findAll() {
 		return service.findAll();
 	}
 
+	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping
 	public void save(@RequestBody Inventory inventory) {
 		service.save(inventory);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN', 'CLIENTE')")
 	@GetMapping("/{id}")
 	public Inventory findById(@PathVariable int id) {
 		return service.findById(id);
--- src/main/java/com/cibertec/api_ventas_ropa/controller/OrderController.java
+++ src/main/java/com/cibertec/api_ventas_ropa/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.cibertec.api_ventas_ropa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,11 +21,13 @@ public class OrderController {
 	private OrderService service;
 	
 	@GetMapping
+	@PreAuthorize("hasAnyRole('ADMIN', 'CLIENTE')")
 	public List<Order> findAll() {
 		return service.findAll();
 	}
 	
 	@PostMapping
+	@PreAuthorize("hasRole('CLIENTE')")
 	public void save(@RequestBody Order order) {
 		service.save(order);
 	}

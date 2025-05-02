17cbf7a79e90653cde5902222d8550efa6b4fc29
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-05-01 22:41:53 -0500

    agregar findByRole en UserRepository

--- src/main/java/com/cibertec/api_ventas_ropa/repository/UserRepository.java
+++ src/main/java/com/cibertec/api_ventas_ropa/repository/UserRepository.java
@@ -4,10 +4,12 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.cibertec.api_ventas_ropa.model.Role;
 import com.cibertec.api_ventas_ropa.model.User;
 import java.util.List;
 
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 	Optional<User> findByEmail(String username);
+	List<User> findByRole(Role role);
 }
